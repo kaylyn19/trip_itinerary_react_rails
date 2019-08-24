c2d7b50ffab66e946b8e155e3a3aeb2eb6f815a0
Kaylyn Kwak <kkwak0803@gmail.com>
2019-08-23 17:56:53 -0700

    fix bugs in homepage slideshow

--- project_react/trip_itinerary_react/src/Components/WelcomePage/index.js
+++ project_react/trip_itinerary_react/src/Components/WelcomePage/index.js
@@ -2,22 +2,19 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 function WelcomePage() {
-    return (//<main className="landing-page">
-        <div className="slider">
-            <slide class="slide"><p class="slide-number">1</p></slide>
-            <slide class="slide"><p class="slide-number">2</p></slide>
-            <slide class="slide"><p class="slide-number">3</p></slide>
-            <slide class="slide"><p class="slide-number">4</p></slide>
-        </div>
-        )
-        {/* <div id="test">    
+    return (<div className="slider">
+        <slide class="slide"><p class="slide-number"></p></slide>
+        <slide class="slide"><p class="slide-number"></p></slide>
+        <slide class="slide"><p class="slide-number"></p></slide>
+        <slide class="slide"><p class="slide-number"></p></slide>
+        <div className='welcome-button'>
             <p className="welcome-text">Plan your next adventure with us!</p>
             <button id="welcome-button">Are you ready? &nbsp;
                 <span><Link to="/sign_in">Start here</Link></span>
             </button>
-        </div> */}
-    // </main>
-    
+        </div>
+    </div>
+    )
 }
 
 export default WelcomePage
